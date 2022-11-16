e144965c269a6815df06d2688cbc0e522b1dce30
N1kf3 <105322440+N1kf3@users.noreply.github.com>
2022-11-16 18:27:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function open_menu(){
+    document.getElementById('Menu_nav').style.visibility= 'visible';
+    document.getElementById('body').style.overflow= 'hidden';
+    
+}
+
+function close_menu(){
+    document.getElementById('Menu_nav').style.visibility= 'hidden';
+    document.getElementById('body').style.overflow= 'visible';
+    
+
+}
